f8af30c50e8085ab1293801324bf8da9a59c23e6
Nguyen Jan <Jan.T.H.Nguyen@googlemail.com>
2020-03-07 09:49:42 +0100

    Base structure

---
+++ Rename/Main.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Rename
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelMatch = new System.Windows.Forms.Label();
+            this.LabelReplace = new System.Windows.Forms.Label();
+            this.TextBoxMatch = new System.Windows.Forms.TextBox();
+            this.TextBoxReplace = new System.Windows.Forms.TextBox();
+            this.ListViewIO = new System.Windows.Forms.ListView();
+            this.ColumnOriginal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnNew = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ButtonClear = new System.Windows.Forms.Button();
+            this.ButtonRename = new System.Windows.Forms.Button();
+            this.CheckBoxEntire = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // LabelMatch
+            // 
+            this.LabelMatch.AutoSize = true;
+            this.LabelMatch.Location = new System.Drawing.Point(12, 9);
+            this.LabelMatch.Name = "LabelMatch";
+            this.LabelMatch.Size = new System.Drawing.Size(40, 13);
+            this.LabelMatch.TabIndex = 0;
+            this.LabelMatch.Text = "Match:";
+            // 
+            // LabelReplace
+            // 
+            this.LabelReplace.AutoSize = true;
+            this.LabelReplace.Location = new System.Drawing.Point(12, 35);
+            this.LabelReplace.Name = "LabelReplace";
+            this.LabelReplace.Size = new System.Drawing.Size(50, 13);
+            this.LabelReplace.TabIndex = 1;
+            this.LabelReplace.Text = "Replace:";
+            // 
+            // TextBoxMatch
+            // 
+            this.TextBoxMatch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxMatch.Location = new System.Drawing.Point(68, 6);
+            this.TextBoxMatch.Name = "TextBoxMatch";
+            this.TextBoxMatch.Size = new System.Drawing.Size(276, 20);
+            this.TextBoxMatch.TabIndex = 2;
+            this.TextBoxMatch.Text = ".";
+            this.TextBoxMatch.TextChanged += new System.EventHandler(this.TextBoxMatch_TextChanged);
+            // 
+            // TextBoxReplace
+            // 
+            this.TextBoxReplace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxReplace.Location = new System.Drawing.Point(68, 32);
+            this.TextBoxReplace.Name = "TextBoxReplace";
+            this.TextBoxReplace.Size = new System.Drawing.Size(335, 20);
+            this.TextBoxReplace.TabIndex = 3;
+            this.TextBoxReplace.Text = "$0";
+            this.TextBoxReplace.TextChanged += new System.EventHandler(this.TextBoxReplace_TextChanged);
+            // 
+            // ListViewIO
+            // 
+            this.ListViewIO.AllowDrop = true;
+            this.ListViewIO.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListViewIO.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnOriginal,
+            this.ColumnNew});
+            this.ListViewIO.FullRowSelect = true;
+            this.ListViewIO.HideSelection = false;
+            this.ListViewIO.Location = new System.Drawing.Point(15, 58);
+            this.ListViewIO.Name = "ListViewIO";
+            this.ListViewIO.Size = new System.Drawing.Size(388, 324);
+            this.ListViewIO.TabIndex = 4;
+            this.ListViewIO.UseCompatibleStateImageBehavior = false;
+            this.ListViewIO.View = System.Windows.Forms.View.Details;
+            this.ListViewIO.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListViewIO_DragDrop);
+            this.ListViewIO.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListViewIO_DragEnter);
+            this.ListViewIO.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ListViewIO_KeyDown);
+            // 
+            // ColumnOriginal
+            // 
+            this.ColumnOriginal.Text = "Original";
+            this.ColumnOriginal.Width = 192;
+            // 
+            // ColumnNew
+            // 
+            this.ColumnNew.Text = "New";
+            this.ColumnNew.Width = 192;
+            // 
+            // ButtonClear
+            // 
+            this.ButtonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ButtonClear.Location = new System.Drawing.Point(15, 388);
+            this.ButtonClear.Name = "ButtonClear";
+            this.ButtonClear.Size = new System.Drawing.Size(75, 23);
+            this.ButtonClear.TabIndex = 5;
+            this.ButtonClear.Text = "Clear";
+            this.ButtonClear.UseVisualStyleBackColor = true;
+            this.ButtonClear.Click += new System.EventHandler(this.ButtonClear_Click);
+            // 
+            // ButtonRename
+            // 
+            this.ButtonRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonRename.Location = new System.Drawing.Point(328, 388);
+            this.ButtonRename.Name = "ButtonRename";
+            this.ButtonRename.Size = new System.Drawing.Size(75, 23);
+            this.ButtonRename.TabIndex = 6;
+            this.ButtonRename.Text = "Rename";
+            this.ButtonRename.UseVisualStyleBackColor = true;
+            this.ButtonRename.Click += new System.EventHandler(this.ButtonRename_Click);
+            // 
+            // CheckBoxEntire
+            // 
+            this.CheckBoxEntire.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CheckBoxEntire.AutoSize = true;
+            this.CheckBoxEntire.Location = new System.Drawing.Point(350, 9);
+            this.CheckBoxEntire.Name = "CheckBoxEntire";
+            this.CheckBoxEntire.Size = new System.Drawing.Size(53, 17);
+            this.CheckBoxEntire.TabIndex = 8;
+            this.CheckBoxEntire.Text = "Entire";
+            this.CheckBoxEntire.UseVisualStyleBackColor = true;
+            this.CheckBoxEntire.CheckedChanged += new System.EventHandler(this.CheckBoxEntire_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 423);
+            this.Controls.Add(this.CheckBoxEntire);
+            this.Controls.Add(this.ButtonRename);
+            this.Controls.Add(this.ButtonClear);
+            this.Controls.Add(this.ListViewIO);
+            this.Controls.Add(this.TextBoxReplace);
+            this.Controls.Add(this.TextBoxMatch);
+            this.Controls.Add(this.LabelReplace);
+            this.Controls.Add(this.LabelMatch);
+            this.MinimumSize = new System.Drawing.Size(256, 256);
+            this.Name = "Main";
+            this.Text = "Rename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelMatch;
+        private System.Windows.Forms.Label LabelReplace;
+        private System.Windows.Forms.TextBox TextBoxMatch;
+        private System.Windows.Forms.TextBox TextBoxReplace;
+        private System.Windows.Forms.ListView ListViewIO;
+        private System.Windows.Forms.ColumnHeader ColumnOriginal;
+        private System.Windows.Forms.ColumnHeader ColumnNew;
+        private System.Windows.Forms.Button ButtonClear;
+        private System.Windows.Forms.Button ButtonRename;
+        private System.Windows.Forms.CheckBox CheckBoxEntire;
+    }
+}
+
---
+++ Rename/Main.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rename
+{
+    public partial class Main : Form
+    {
+        const string ErrorName = "?";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewIO_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) 
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void ListViewIO_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (!ContainsFile(file))
+                {
+                    string fileName = Path.GetFileName(file);
+                    ListViewItem item = new ListViewItem(new string[] { fileName, FileRegexReplace(fileName) })
+                    {
+                        Tag = file
+                    };
+                    ListViewIO.Items.Add(item);
+                }
+            }
+        }
+
+        private void ListViewIO_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                foreach (ListViewItem listViewItem in ListViewIO.SelectedItems)
+                {
+                    listViewItem.Remove();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check if file was already added
+        /// </summary>
+        /// <param name="file">Full path of file</param>
+        /// <returns>true iff file was already added</returns>
+        private bool ContainsFile(string file)
+        {
+            foreach (ListViewItem item in ListViewIO.Items)
+            {
+                if ((string)item.Tag == file)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Removes a file from ListViewIO
+        /// </summary>
+        /// <param name="file">Full path of file</param>
+        private void RemoveFile(string file)
+        {
+            foreach (ListViewItem item in ListViewIO.Items)
+            {
+                if ((string)item.Tag == file)
+                {
+                    item.Remove();
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recalculates the output names of all elements in ListViewIO
+        /// </summary>
+        private void RefreshOut()
+        {
+            foreach(ListViewItem item in ListViewIO.Items)
+            {
+                item.SubItems[1].Text = FileRegexReplace(item.SubItems[0].Text);
+            }
+        }
+
+        /// <summary>
+        /// Apply the expression in TextBoxMatch and TextBoxReplace on Input
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns>Input replaced using the given regular expressions</returns>
+        private string FileRegexReplace(string input)
+        {
+            try
+            {
+                string rgxIn = CheckBoxEntire.Checked ? EncloseRegex(TextBoxMatch.Text) : TextBoxMatch.Text;
+                if (Regex.IsMatch(input, rgxIn) || !CheckBoxEntire.Checked)
+                {
+                    return Regex.Replace(input, rgxIn, TextBoxReplace.Text);
+                }
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            return ErrorName;
+        }
+
+        /// <summary>
+        /// Checks for a valid regex match
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns>true iff non-entire match or match successfull</returns>
+        private bool FileRegexMatch(string input)
+        {
+            return !CheckBoxEntire.Checked || Regex.IsMatch(input, EncloseRegex(TextBoxMatch.Text));
+        }
+
+        /// <summary>
+        /// Enclose the regex with a ^ and a $
+        /// </summary>
+        /// <param name="rgx">Regex to enclose</param>
+        /// <returns>^rgx$</returns>
+        private string EncloseRegex(string rgx)
+        {
+            if (!rgx.StartsWith("^"))
+                rgx = "^" + rgx;
+            if (!rgx.EndsWith("$"))
+                rgx += "$";
+
+            return rgx;
+        }
+
+        private void TextBoxMatch_TextChanged(object sender, EventArgs e)
+        {
+            RefreshOut();
+        }
+
+        private void TextBoxReplace_TextChanged(object sender, EventArgs e)
+        {
+            RefreshOut();
+        }
+
+        private void CheckBoxEntire_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshOut();
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            ListViewIO.Items.Clear();
+        }
+
+        private void ButtonRename_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> moveDict = new Dictionary<string, string>();
+            HashSet<string> usedNames = new HashSet<string>();
+            foreach (ListViewItem item in ListViewIO.Items)
+            {
+                string file = (string)item.Tag;
+                string dir = Path.GetDirectoryName(file);
+                string oldName = item.SubItems[0].Text;
+                string newName = item.SubItems[1].Text;
+                string target = Path.Combine(dir, newName);
+
+                moveDict[file] = target;
+
+                // check for matching error
+                if (!FileRegexMatch(oldName))
+                {
+                    MessageBox.Show("Can not match '" + oldName + "'.", "Matching error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // check for collisions
+                if (!usedNames.Add(target))
+                {
+                    MessageBox.Show("Can not move file '" + file + "' to '" + target + "'.", "Collision error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                usedNames.Add(file);
+            }
+
+            foreach (KeyValuePair<string, string> entry in moveDict)
+            {
+                if (entry.Key != entry.Value)
+                {
+                    try
+                    {
+                        if (Directory.Exists(entry.Key))
+                        {
+                            Directory.Move(entry.Key, entry.Value);
+                        }
+                        else
+                        {
+                            File.Move(entry.Key, entry.Value);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (MessageBox.Show("Can not move '" + entry.Key + "' to '" + entry.Value + "'. " + ex.Message, "Exception", MessageBoxButtons.OKCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
+                            return;
+                    }
+                }
+
+                RemoveFile(entry.Key);
+            }
+        }
+    }
+}
